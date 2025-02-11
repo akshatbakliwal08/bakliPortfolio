2c00927b0861fea55480e294124a4a46600e90f9
Akshat Bakliwal <abakliw1@asu.edu>
2025-02-11 10:57:04 -0800

    Added Photo

--- src/data/siteMetaData.mjs
+++ src/data/siteMetaData.mjs
@@ -10,7 +10,7 @@ export const siteMetadata = {
   siteRepo: "https://github.com/akshatbakliwal08/bakliPortfolio",
   siteLogo: "/favicon.ico",
   image: "/static/homepage.png",
-  resume:"/resume/AkshatBakliwalResume_Final.pdf",
+  resume:"/resume/AkshatBakliwalResumeSE.pdf",
   twitterImage: "/static/homepage.png",
   socialBanner: "/static/homepage.png",
   email: "abakliw1@asu.edu",
