d27a4becd81567d76086db8627b01261694dde20
Akshat Bakliwal <abakliw1@asu.edu>
2025-02-10 22:52:49 -0800

    Added Photo

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -1,4 +1,4 @@
-import ContactButton from "@/components/contact-form/contact-button";
+// import ContactButton from "@/components/contact-form/contact-button";
 import { GithubIcon, LinkedinIcon } from "@/components/icons";
 import { siteMetadata } from "@/data/siteMetaData.mjs";
 
@@ -18,9 +18,9 @@ export default function Footer() {
         >
           <span>abakliw1@asu.edu</span>
         </a>
-        <div className="flex justify-center">
+        {/* <div className="flex justify-center">
           <ContactButton />
-        </div>
+        </div> */}
       </div>
       <div className="flex w-full flex-col items-center justify-between gap-8 text-center  md:flex-row md:justify-between lg:mx-auto lg:max-w-7xl">
         <span className="text-foreground">©2025 Akshat Bakliwal</span>
